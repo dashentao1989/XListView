bcbb9ce0c1ea5ccef688ebda5e5af96afe6f0bd6
jamestao <495428527qq.com>
2015-05-05 16:34:54 +0800

    th first commit

---
+++ src/me/maxwin/view/Demo1.java
@@ -0,0 +1,41 @@
+package me.maxwin.view;
+
+public class Demo1 {
+	private String a;
+	private String b;
+	private boolean c;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((a == null) ? 0 : a.hashCode());
+		result = prime * result + ((b == null) ? 0 : b.hashCode());
+		result = prime * result + (c ? 1231 : 1237);
+		return result;
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Demo1 other = (Demo1) obj;
+		if (a == null) {
+			if (other.a != null)
+				return false;
+		} else if (!a.equals(other.a))
+			return false;
+		if (b == null) {
+			if (other.b != null)
+				return false;
+		} else if (!b.equals(other.b))
+			return false;
+		if (c != other.c)
+			return false;
+		return true;
+	}
+}
